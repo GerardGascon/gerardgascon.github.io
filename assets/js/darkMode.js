857eae63fae2c2c8677ff1634db8f3e383f1270d
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-01-01 23:48:08 +0100

    Updated dark mode detection

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,22 +1,20 @@
 var html = document.documentElement;
 
+if (localStorage.getItem("pref-theme") === "dark") {
+    html.setAttribute('data-mode', 'dark');
+} else if (localStorage.getItem("pref-theme") === "light") {
+    html.removeAttribute('data-mode');
+} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    html.setAttribute('data-mode', 'dark');
+}
 function toggleMode() {
     var hasAttribute = html.hasAttribute('data-mode');
-    var value = hasAttribute ? html.getAttribute('data-mode') : null;
 
     if (hasAttribute) {
-        if (value == "light") {
-            html.setAttribute('data-mode', 'dark');
-        } else {
-            html.setAttribute('data-mode', 'light');
-        }
+        html.removeAttribute('data-mode');
+        localStorage.setItem("pref-theme", 'light');
     } else {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            html.setAttribute('data-mode', 'dark');
-        } else {
-            html.setAttribute('data-mode', 'light');
-        }
+        html.setAttribute('data-mode', 'dark');
+        localStorage.setItem("pref-theme", 'dark');
     }
 }
-
-toggleMode();
